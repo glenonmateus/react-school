6bdbc7a4bea3061991e05427bb0459a145c99c93
Glenon Mateus <glenonmateus+github@gmail.com>
2026-07-13 23:49:29 -0300

    refact: protect some route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,22 @@
 import Home from "pages/Home";
+import Login from "pages/Login";
 import NotFound from "pages/NotFound";
+import Photos from "pages/Photos";
+import Register from "pages/Register";
+import Student from "pages/Student";
 import { Route, Routes } from "react-router";
 import ProtectedRoute from "routes/ProtectedRoute";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route element={<ProtectedRoute />}>
-        <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/login" element={<Login></Login>}></Route>
+      <Route path="/register" element={<Register></Register>}></Route>
+      <Route element={<ProtectedRoute isClosed />}>
+        <Route path="/student/:id/edit" element={<Student></Student>}></Route>
+        <Route path="/student" element={<Student></Student>}></Route>
+        <Route path="/photos/:id" element={<Photos></Photos>}></Route>
       </Route>
       <Route path="*" element={<NotFound></NotFound>}></Route>
     </Routes>
