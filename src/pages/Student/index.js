18e489693b1f580ea221414584af5675e3eadb07
Glenon Mateus <glenonmateus+github@gmail.com>
2026-07-13 23:48:12 -0300

    feat: add Student page

---
+++ src/pages/Student/index.js
@@ -0,0 +1,11 @@
+import { Container } from "styles/GlobalStyles";
+
+const Student = () => {
+  return (
+    <Container>
+      <h1>Student</h1>
+    </Container>
+  );
+};
+
+export default Student;
