eaae9e140578df2c09ae3d704b7cbd56d871b9d1
Glenon Mateus <glenonmateus+github@gmail.com>
2026-07-14 19:35:42 -0300

    feat: add Login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,60 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import * as actions from "store/modules/auth/actions";
 import { Container } from "styles/GlobalStyles";
+import { Form } from "./styled";
 
 const Login = () => {
+  const dispatch = useDispatch();
+
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setForm((prevData) => {
+      return {
+        ...prevData,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    dispatch(actions.loginRequest(form));
+  };
+
   return (
     <Container>
       <h1>Login</h1>
+      <Form onSubmit={handleSubmit}>
+        <label htmlFor="email" hidden={true}>
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Seu email"
+          value={form.email}
+          onChange={handleChange}
+        />
+        <label htmlFor="password" hidden={true}>
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Sua senha"
+          minLength={6}
+          maxLength={50}
+          value={form.password}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Entrar</button>
+      </Form>
     </Container>
   );
 };
