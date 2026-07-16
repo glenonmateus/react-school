4420b4452d244324c20dd388b9c13c4fb4637a57
Glenon Mateus <glenonmateus+github@gmail.com>
2026-07-16 16:00:10 -0300

    feat: add axios interceptor, add header authorization bearer token

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,5 +1,22 @@
 import axios from "axios";
+import store from "store";
 
-export default axios.create({
+const api = axios.create({
   baseURL: process.env.REACT_APP_AXIOS_BASE_URL,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const state = store.getState();
+    const token = state.auth?.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export default api;
