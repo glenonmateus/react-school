51ddc09e21f0b925647abdcf6e9f4de88bf0baef
Glenon Mateus <glenonmateus+github@gmail.com>
2026-07-16 14:06:27 -0300

    refact: add user initialState

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -4,23 +4,29 @@ const initialState = {
   isLoggedIn: false,
   token: false,
   isLoading: false,
+  user: {},
 };
 
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
+  const { type, payload } = action;
+
+  switch (type) {
     case types.LOGIN_SUCCESS: {
       const newState = { ...state };
       newState.isLoggedIn = true;
-      newState.token = action.payload.access_token;
+      newState.token = payload.access_token;
       newState.isLoading = false;
       return newState;
     }
 
     case types.LOGIN_FAILURE: {
-      return initialState;
+      const newState = { ...initialState };
+      newState.isLoading = false;
+      return newState;
     }
 
     case types.LOGIN_REQUEST: {
+      console.log("loginRequest", payload);
       const newState = { ...state };
       newState.isLoading = true;
       return newState;
