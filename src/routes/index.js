3a4a186112b39b8585c85c78fc77e04a259d716e
Glenon Mateus <glenonmateus+github@gmail.com>
2026-07-24 00:22:49 -0300

    refactor: use StudentForm page

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import Home from "pages/Home";
 import Login from "pages/Login";
 import NotFound from "pages/NotFound";
 import Register from "pages/Register";
-import StoreStudent from "pages/StoreStudent";
+import StudentForm from "pages/StudentForm";
 import { Route, Routes } from "react-router";
 import ProtectedRoute from "routes/ProtectedRoute";
 
@@ -13,7 +13,11 @@ const AppRoutes = () => {
       <Route path="/register" element={<Register></Register>}></Route>
       <Route element={<ProtectedRoute isClosed />}>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/student" element={<StoreStudent></StoreStudent>}></Route>
+        <Route path="/student" element={<StudentForm></StudentForm>}></Route>
+        <Route
+          path="/student/:id/update"
+          element={<StudentForm></StudentForm>}
+        ></Route>
       </Route>
       <Route path="*" element={<NotFound></NotFound>}></Route>
     </Routes>
