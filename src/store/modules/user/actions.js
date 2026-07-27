67ab5490331710d39c9afe9d05686e5e3fd7b8ef
Glenon Mateus <glenonmateus+github@gmail.com>
2026-07-27 17:28:26 -0300

    feat: add fetch and update user

--- src/store/modules/user/actions.js
+++ src/store/modules/user/actions.js
@@ -9,3 +9,23 @@ export const storeUserSuccess = () => {
 export const storeUserFailure = () => {
   return { type: types.STORE_USER_FAILURE };
 };
+
+export const fetchUserRequest = (payload) => {
+  return { type: types.FETCH_USER_REQUEST, payload };
+};
+export const fetchUserSuccess = () => {
+  return { type: types.FETCH_USER_SUCCESS };
+};
+export const fetchUserFailure = () => {
+  return { type: types.FETCH_USER_FAILURE };
+};
+
+export const updateUserRequest = (payload) => {
+  return { type: types.UPDATE_USER_REQUEST, payload };
+};
+export const updateUserSuccess = () => {
+  return { type: types.UPDATE_USER_SUCCESS };
+};
+export const updateUserFailure = () => {
+  return { type: types.UPDATE_USER_FAILURE };
+};
