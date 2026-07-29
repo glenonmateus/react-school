85cb244c8ef5cd220c8fa52dbdcfa1fbdfbf14c0
Glenon Mateus <glenonmateus+github@gmail.com>
2026-07-29 12:06:50 -0300

    feat: add fetch and update user

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -30,4 +30,52 @@ function* storeUserRequest({ payload }) {
   }
 }
 
-export default all([takeLatest(types.STORE_USER_REQUEST, storeUserRequest)]);
+const axiosUpdateUser = async (payload) => {
+  const { name, surname, email, password } = payload;
+  try {
+    return await axios.put(`/users/`, {
+      name,
+      surname,
+      email,
+      password,
+    });
+  } catch (error) {
+    handleAxiosError(error);
+  }
+};
+
+function* updateUserRequest({ payload }) {
+  const { navigate } = payload;
+  try {
+    yield call(axiosUpdateUser, payload);
+    yield put(actions.updateUserSuccess());
+    toast.success("Conta atualizada com sucesso!");
+    navigate("/", { replace: true });
+  } catch {
+    yield put(actions.updateUserFailure());
+  }
+}
+
+const axiosFetchUser = async (payload) => {
+  const { id } = payload;
+  try {
+    return await axios.get(`/users/${id}`);
+  } catch (error) {
+    handleAxiosError(error);
+  }
+};
+
+function* fetchUserRequest({ payload }) {
+  try {
+    const response = yield call(axiosFetchUser, payload);
+    yield put(actions.fetchUserSuccess(response.data));
+  } catch {
+    yield put(actions.fetchUserFailure());
+  }
+}
+
+export default all([
+  takeLatest(types.STORE_USER_REQUEST, storeUserRequest),
+  takeLatest(types.UPDATE_STUDENT_REQUEST, updateUserRequest),
+  takeLatest(types.FETCH_USER_REQUEST, fetchUserRequest),
+]);
